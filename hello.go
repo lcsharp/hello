635405aecb8d6f7b3886eb411810679aa11f72c5
Chris Sharp <lcsharp@us.ibm.com>
2016-05-06 21:36:24 -0400

    added reverse

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	
+	"github.com/lcsharp/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, cold cruel world.\n")
+	fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
 }
